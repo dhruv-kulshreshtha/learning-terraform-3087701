3f2844d45b0d96777fd57d457b6c69642e54b57f
dhruv-kulshreshtha <46960599+dhruv-kulshreshtha@users.noreply.github.com>
2026-03-31 17:22:45 +0530

    remove attachment

--- main.tf
+++ main.tf
@@ -78,12 +78,6 @@ resource "aws_lb_target_group" "blog" {
   vpc_id   = module.blog_vpc.vpc_id
 }
 
-resource "aws_lb_target_group_attachment" "blog" {
-  target_group_arn = aws_lb_target_group.blog.arn
-  target_id        = aws_instance.blog.id
-  port             = 80
-}
-
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "9.2.0"
@@ -100,7 +94,7 @@ module "blog_autoscaling" {
   instance_type        = var.instance_type
   image_id             = data.aws_ami.app_ami.id
 
-  traffic_source_attachements = {
+  traffic_source_attachments = {
     blog_alb = {
       traffic_source_identifier = aws_lb_target_group.blog.arn
     }
